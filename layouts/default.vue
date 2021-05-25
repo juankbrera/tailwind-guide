b9b674f1b3f1efc44c6e2ca8320dfcf525d07038
jcabrera <jcabrera>
2021-05-25 17:15:51 -0600

    creating nuxt project

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Hola
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
+
+html {
+  -moz-osx-font-smoothing: grayscale;
+  -ms-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: 100%;
+  box-sizing: border-box;
+  font-family: 'Nunito', sans-serif;
+  font-size: 14px;
+  word-spacing: 1px;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
