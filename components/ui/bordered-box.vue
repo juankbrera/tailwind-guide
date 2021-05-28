2e77965d150df76c5b803ff1f540d6ac266d40f3
jcabrera <jcabrera>
2021-05-27 20:53:43 -0600

    Adding components

---
+++ components/ui/bordered-box.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-white border-2 border-black rounded-xl p-6 shadow-lg">
+    <slot />
+  </div>
+</template>
---
+++ components/ui/section/title.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="border-b-2 border-black py-6 mb-6">
+    <slot />
+  </h1>
+</template>
