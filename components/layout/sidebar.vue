28a3608fe0ce773f35804567eebb3a354029ced5
jcabrera <jcabrera>
2021-05-27 20:53:23 -0600

    Updating layout

---
+++ components/layout/sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <ui-bordered-box>
+    <p class="font-bold mb-2">Concepts</p>
+    <ul class="ml-4 list-disc list-inside">
+      <li><a href="" class="hover:underline">What's Tailwind CSS?</a></li>
+      <li><a href="" class="hover:underline">Utility-first CSS</a></li>
+    </ul>
+
+    <p class="font-bold mt-6 mb-2">Commonly Used</p>
+    <ul class="ml-4 list-disc list-inside">
+      <li><a href="" class="hover:underline">Typography</a></li>
+      <li><a href="" class="hover:underline">Colors</a></li>
+      <li><a href="" class="hover:underline">Borders</a></li>
+      <li><a href="" class="hover:underline">Sizing</a></li>
+      <li><a href="" class="hover:underline">Spacing</a></li>
+      <li><a href="" class="hover:underline">Breakpoints</a></li>
+      <li><a href="" class="hover:underline">States</a></li>
+    </ul>
+
+    <p class="font-bold mt-6 mb-2">Advance CSS concepts</p>
+    <ul class="ml-4 list-disc list-inside">
+      <li><a href="" class="hover:underline">Inline vs Block</a></li>
+      <li><a href="" class="hover:underline">Flexbox</a></li>
+      <li><a href="" class="hover:underline">Grid</a></li>
+    </ul>
+  </ui-bordered-box>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,13 @@
 <template>
-  <Nuxt />
+  <div>
+    <layout-header />
+    <div class="container mx-auto p-6 grid grid-cols-1 lg:grid-cols-4 gap-8">
+      <div class="col-span-2 lg:col-span-1">
+        <layout-sidebar />
+      </div>
+      <div class="col-span-2 lg:col-span-3">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
 </template>
