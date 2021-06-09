373029ebd88e465df61d951db4ca96c4f771c0e5
jcabrera <jcabrera>
2021-06-08 22:47:14 -0600

    adding examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,19 @@ module.exports = {
         laptop: '1024px',
         desktop: '1280px',
       },
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
     },
   },
   variants: {
     extend: {
       backgroundColor: ['active'],
+      opacity: ['disabled'],
     },
   },
   plugins: [],
